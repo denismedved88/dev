80ccdede0320a148b5e4512e28ffa305c8949b24
denismedved88 <denismedved88@gmail.com>
2025-12-26 21:00:27 +0300

    Update check_password.py

--- check_password.py
+++ check_password.py
@@ -1,43 +1,29 @@
 def has_digit(password):
     if len(password) >= 12:
-        print("Пароль достаточно длинный")
         return 2
-    print("Пароль короткий")
     return 0
 
 
 def check_digits(password):
     if any(variable.isdigit() for variable in password):
-        print("Пароль содержит цифры")
         return 2
-    print("Пароль не содержит цифр")
     return 0
 
 
-def has_letters(password):
-    if any(variable.isalpha() for variable in password):
-        print("Пароль содержит буквы")
+def has_upper_letters(password):
+    if any(variable.isupper() for variable in password):
         return 2
-    print("Пароль не содержит букв")
     return 0
 
-
-def has_upper_and_lower_letters(password):
-    has_upper = any(variable.isupper() for variable in password)
-    has_lower = any(variable.islower() for variable in password)
-
-    if has_upper and has_lower:
-        print("Пароль содержит и большие, и маленькие буквы")
+def has_lower_letters(password):
+    if any(variable.islower() for variable in password):
         return 2
-    print("Пароль должен содержать буквы разного регистра")
     return 0
 
 
 def has_symbols(password):
     if any(not variable.isalnum() for variable in password):
-        print("Пароль содержит специальные символы")
         return 2
-    print("Пароль не содержит специальных символов")
     return 0
 
 
@@ -48,8 +34,8 @@ def main():
     check_points = [
         has_digit,
         check_digits,
-        has_letters,
-        has_upper_and_lower_letters,
+        has_upper_letters,
+        has_lower_letters,
         has_symbols
     ]
 
